b1c095a8cbcf2e0d6e21ee55841ae67bf2beef7f
Pranav Pokale <pranavpokale50034@gmail.com>
2024-06-12 22:28:23 +0530

    deploy file update

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v22.2.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v22.3.0/bin
 
 cd week-9
- git pull origin2 master
+ git pull origin master
  cd server
  pm2 kill
  pm2 start index.js
--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "sample-deploy.pem" ubuntu@ec2-13-51-167-149.eu-north-1.compute.amazonaws.com	  "sudo bash ~/deploy.sh"
+ssh -t -i "sample-deploy.pem" ubuntu@ec2-13-60-36-106.eu-north-1.compute.amazonaws.com "sudo bash ~/deploy.sh"
