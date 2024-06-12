3c379e2eebb7f5175b81226f658fc4557edee18b
Pranav Pokale <pranavpokale50034@gmail.com>
2024-06-12 20:33:08 +0530

    final update

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+ssh -t -i "sample-deploy.pem" ubuntu@ec2-13-51-167-149.eu-north-1.compute.amazonaws.com	  "sudo bash ~/deploy.sh"
--- server/index.js
+++ server/index.js
@@ -12,9 +12,13 @@ app.use(express.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>");
+})
+
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+// mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
